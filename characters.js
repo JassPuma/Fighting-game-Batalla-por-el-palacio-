a1355afda59d85b4aa8557b2f13b6f69981e11a5
JassPuma <pumajacer@gmail.com>
2025-10-15 16:23:39 -0500

    Update characters.js

--- characters.js
+++ characters.js
@@ -1,7 +1,18 @@
-// Lista de personajes con nombre y sprite
+// Lista completa de 15 personajes
 const characters = [
-  { name: 'Keiko', sprite: 'assets/images/sprites/keiko_idle.png' },
-  { name: 'Pedro', sprite: 'assets/images/sprites/pedro_idle.png' },
-  { name: 'Juan', sprite: 'assets/images/sprites/juan_idle.png' }
-  // Agrega los demás personajes
+  { name: 'Keiko Fujimori', sprite: 'assets/images/sprites/keiko.png' },
+  { name: 'Pedro Castillo', sprite: 'assets/images/sprites/pedro.png' },
+  { name: 'Juan Velasco', sprite: 'assets/images/sprites/juan.png' },
+  { name: 'Alan García', sprite: 'assets/images/sprites/alan.png' },
+  { name: 'Ollanta Humala', sprite: 'assets/images/sprites/ollanta.png' },
+  { name: 'Susana Villarán', sprite: 'assets/images/sprites/susana.png' },
+  { name: 'Alberto Fujimori', sprite: 'assets/images/sprites/alberto.png' },
+  { name: 'Martín Vizcarra', sprite: 'assets/images/sprites/vizcarra.png' },
+  { name: 'Alejandro Toledo', sprite: 'assets/images/sprites/toledo.png' },
+  { name: 'César Acuña', sprite: 'assets/images/sprites/acuna.png' },
+  { name: 'Keiko 2', sprite: 'assets/images/sprites/keiko2.png' }, // si hay variación
+  { name: 'Rafael López Aliaga', sprite: 'assets/images/sprites/lopez_aliaga.png' },
+  { name: 'Verónika Mendoza', sprite: 'assets/images/sprites/vera.png' },
+  { name: 'Julio Guzmán', sprite: 'assets/images/sprites/julio.png' },
+  { name: 'George Forsyth', sprite: 'assets/images/sprites/george.png' }
 ];
