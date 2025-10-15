1121adf1a9604f6f4baa67b16cb3053b2470d60e
JassPuma <pumajacer@gmail.com>
2025-10-15 16:21:47 -0500

    Update characters.js

--- characters.js
+++ characters.js
@@ -1,18 +1,7 @@
-// js/characters.js
-export const characters = {
-  alan: { name: "Alan García", color: "red", weapon: "revólver", special: "disparo final", specialPower: 25 },
-  keiko: { name: "Keiko Fujimori", color: "orange", weapon: "táper", special: "lluvia de tápers", specialPower: 20 },
-  fujimori: { name: "Alberto Fujimori", color: "yellow", weapon: "katana", special: "golpe de dictador", specialPower: 30 },
-  toledo: { name: "Alejandro Toledo", color: "brown", weapon: "botella de vino", special: "borrachazo ancestral", specialPower: 22 },
-  ollanta: { name: "Ollanta Humala", color: "green", weapon: "puños militares", special: "grito nacionalista", specialPower: 24 },
-  castillo: { name: "Pedro Castillo", color: "white", weapon: "sombrero mágico", special: "tiza de la justicia", specialPower: 18 },
-  veronika: { name: "Verónika Mendoza", color: "purple", weapon: "rosa socialista", special: "torbellino progresista", specialPower: 20 },
-  acuña: { name: "César Acuña", color: "blue", weapon: "maletín de plata", special: "dinero volador", specialPower: 26 },
-  lopez: { name: "Rafael López Aliaga", color: "darkblue", weapon: "cruz gigante", special: "exorcismo empresarial", specialPower: 28 },
-  desoto: { name: "Hernando de Soto", color: "gray", weapon: "libro de economía", special: "teoría de choque", specialPower: 23 },
-  lescano: { name: "Yonhy Lescano", color: "lightblue", weapon: "megáfono", special: "voz del pueblo", specialPower: 21 },
-  forsyt: { name: "George Forsyth", color: "lightgreen", weapon: "tarjeta roja", special: "expulsión total", specialPower: 20 },
-  antauro: { name: "Antauro Humala", color: "maroon", weapon: "fusil etnocacerista", special: "levantamiento andino", specialPower: 29 },
-  guzman: { name: "Julio Guzmán", color: "pink", weapon: "tablet rota", special: "descarga eléctrica", specialPower: 22 }
-};
-
+// Lista de personajes con nombre y sprite
+const characters = [
+  { name: 'Keiko', sprite: 'assets/images/sprites/keiko_idle.png' },
+  { name: 'Pedro', sprite: 'assets/images/sprites/pedro_idle.png' },
+  { name: 'Juan', sprite: 'assets/images/sprites/juan_idle.png' }
+  // Agrega los demás personajes
+];
