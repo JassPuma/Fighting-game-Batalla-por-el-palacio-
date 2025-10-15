eee7f02263e40a1d486134d72907db3373b5becd
JassPuma <pumajacer@gmail.com>
2025-10-15 13:00:34 -0500

    Update characters.js

--- characters.js
+++ characters.js
@@ -1,45 +1,18 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Selecciona tu personaje</title>
-<link rel="stylesheet" href="style.css">
-</head>
-<body>
+// js/characters.js
+export const characters = {
+  alan: { name: "Alan García", color: "red", weapon: "revólver", special: "disparo final", specialPower: 25 },
+  keiko: { name: "Keiko Fujimori", color: "orange", weapon: "táper", special: "lluvia de tápers", specialPower: 20 },
+  fujimori: { name: "Alberto Fujimori", color: "yellow", weapon: "katana", special: "golpe de dictador", specialPower: 30 },
+  toledo: { name: "Alejandro Toledo", color: "brown", weapon: "botella de vino", special: "borrachazo ancestral", specialPower: 22 },
+  ollanta: { name: "Ollanta Humala", color: "green", weapon: "puños militares", special: "grito nacionalista", specialPower: 24 },
+  castillo: { name: "Pedro Castillo", color: "white", weapon: "sombrero mágico", special: "tiza de la justicia", specialPower: 18 },
+  veronika: { name: "Verónika Mendoza", color: "purple", weapon: "rosa socialista", special: "torbellino progresista", specialPower: 20 },
+  acuña: { name: "César Acuña", color: "blue", weapon: "maletín de plata", special: "dinero volador", specialPower: 26 },
+  lopez: { name: "Rafael López Aliaga", color: "darkblue", weapon: "cruz gigante", special: "exorcismo empresarial", specialPower: 28 },
+  desoto: { name: "Hernando de Soto", color: "gray", weapon: "libro de economía", special: "teoría de choque", specialPower: 23 },
+  lescano: { name: "Yonhy Lescano", color: "lightblue", weapon: "megáfono", special: "voz del pueblo", specialPower: 21 },
+  forsyt: { name: "George Forsyth", color: "lightgreen", weapon: "tarjeta roja", special: "expulsión total", specialPower: 20 },
+  antauro: { name: "Antauro Humala", color: "maroon", weapon: "fusil etnocacerista", special: "levantamiento andino", specialPower: 29 },
+  guzman: { name: "Julio Guzmán", color: "pink", weapon: "tablet rota", special: "descarga eléctrica", specialPower: 22 }
+};
 
-<h1>Selecciona tu personaje</h1>
-
-<div class="characters" id="charactersContainer"></div>
-
-<script type="module">
-import { characters } from './js/characters.js';
-
-const container = document.getElementById('charactersContainer');
-
-// Generar los personajes dinámicamente
-for (const key in characters) {
-    const char = characters[key];
-    const div = document.createElement('div');
-    div.classList.add('character');
-    div.dataset.name = key;
-    div.style.border = `2px solid ${char.color}`;
-    div.style.color = char.color;
-    div.innerHTML = `
-        <span>${char.name}</span>
-        <p>Arma: ${char.weapon}</p>
-        <p>Especial: ${char.special} (${char.specialPower})</p>
-    `;
-    container.appendChild(div);
-
-    // Evento click para guardar selección y pasar a game.html
-    div.addEventListener('click', () => {
-        localStorage.setItem('selectedCharacter', key);
-        alert(`Has seleccionado a ${char.name}`);
-        window.location.href = 'game.html';
-    });
-}
-</script>
-
-</body>
-</html>
